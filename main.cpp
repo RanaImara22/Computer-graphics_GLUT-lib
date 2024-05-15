97a66d86a9589f0083be6925507cb79bf5fe5e4a
RoaaEmadEldin <roaa20102002@gmail.com>
2024-05-16 02:43:43 +0300

    Added Bicycle and animation for its rotation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,53 @@
-#include <C:\\MinGW\\include\\GL/glut.h>
+//#include <C:\\MinGW\\include\\GL/glut.h>
+#include <D:\\Programs\\MinGW\\MinGW\\include\\GL/glut.h>
+
+
 void init() {
     glEnable(GL_DEPTH_TEST);//3D
     glClearColor(0.0f, 0.3f, 0.0f, 1.0f); //green
     glMatrixMode(GL_PROJECTION);
     gluPerspective(45.0f, 1.0f, 1.0f, 100.0f);
 }
+
+bool animationEnabled = false;
+float animationAngle = 0.0f;    // Angle for animation
+float bicycleX = 0.0f;          // Bicycle's x-coordinate
+float rightWheelAngle = 0.0f;
+float leftWheelAngle = 0.0f;
+GLfloat bicycleRotation = 0.0f;   // Bicycle's rotation angle
 float windowAngleFirstFloor = 0.0f;// Initially closed
 float windowAngleSecondFloor = 0.0f;
+void zoomCamera(unsigned char key, int x, int y);
+
+void handleKeyPress(unsigned char key, int x, int y) {
+    switch (key) {
+        case 'f': // Move forward
+            bicycleX += 0.1f;
+            break;
+        case 'b': // Move backward
+            bicycleX -= 0.1f;
+            break;
+        case 'r': // Steer right
+            rightWheelAngle += 5.0f; // Adjust the angle as needed
+            break;
+        case 'l': // Steer left
+            leftWheelAngle += 5.0f; // Adjust the angle as needed
+            break;
+    }
+    glutPostRedisplay(); // Redraw the scene with the new transformations
+}
+
+void mouse(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        animationEnabled = true;
+//        glutPostRedisplay(); // Redraw the scene with updated camera position
+//        std::cout << "Left mouse button clicked" << std::endl;
+    } else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+        animationEnabled = false;
+//        std::cout << "Right mouse button clicked" << std::endl;
+    }
+}
+
 // Function to handle key presses for windows
 void window_keyPress(unsigned char key, int x, int y) {
     switch (key) {
@@ -22,7 +63,6 @@ void window_keyPress(unsigned char key, int x, int y) {
     glutPostRedisplay(); // Redraw the scene with updated angles
 }
 
-
 float doorAngle = 0.0f; // Initially, the door is closed
 void door_keyPress(unsigned char key, int x, int y) {
     switch (key) {
@@ -35,11 +75,14 @@ void door_keyPress(unsigned char key, int x, int y) {
     }
     glutPostRedisplay();// Redraw the scene with updated angles
 }
+
 void keyPress(unsigned char key, int x, int y){
     door_keyPress( key,  x,  y);
     window_keyPress( key,  x,  y);
-
+    handleKeyPress( key,  x,  y);
+    zoomCamera(key, x, y);
 }
+
 void drawDoor(float x, float y, float z, float angle) {
     glColor3f(0.3f, 0.2f, 0.2f); // Set the color of the door
     glPushMatrix();
@@ -49,6 +92,16 @@ void drawDoor(float x, float y, float z, float angle) {
     glTranslatef(0.05f, 0.0f, 0.0f); // Move back to the original position
     glScalef(0.1f, 0.2f, 0.01f); // Scale to make it look like a door
     glutSolidCube(1.0f); // Draw a solid cube for the door
+    glPopMatrix();
+
+    // Draw Shadow for the door
+    glColor3f(0.2f, 0.1f, 0.1f);
+    glPushMatrix();
+    glTranslatef(x, y, z);
+    glTranslatef(-0.05f, 0.0f, 0.0f);
+    glTranslatef(0.05f, 0.0f, 0.0f);
+    glScalef(0.1f, 0.2f, 0);
+    glutSolidCube(1.0f);
     glPopMatrix();
 }
 
@@ -61,13 +114,84 @@ void drawWindow(float x, float y, float z, float angle) {
     glScalef(0.1f, 0.1f, 0.01f); // Scale to make it look like a window
     glutSolidCube(1.0f); // Draw a solid cube for the window
     glPopMatrix();
+
+    // Draw Shadow for the window
+    glColor3f(0.2f, 0.2f, 0.3f);
+    glPushMatrix();
+    glTranslatef(x - 0.05f, y, z);
+    glTranslatef(0.05f, 0.0f, 0.0f);
+    glScalef(0.1f, 0.1f, 0);
+    glutSolidCube(1.0f);
+    glPopMatrix();
 }
+
 void drawGround() {
-    glColor3f(0.1f, 0.1f, 0.1f); // Set the color of the ground to black
+    glColor3f(0.6f, 0.6f, 0.6f); // Set the color of the ground to black
     glPushMatrix();
     glTranslatef(0.0f, -0.05f, 0.0f); // Position the ground slightly below the building base
     glScalef(3.0f, 0.1f, 3.0f); // Scale to make it flat and wide
     glutSolidCube(1.0f); // Draw a solid cube for the ground
+    glPopMatrix();
+}
+
+void drawBicycle() {
+    // Set the color of the bicycle frame to brown
+    glColor3f(0.22f, 0.18f, 0.13f);
+
+    // Draw the frame of the bicycle
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glTranslatef(0.01f, 0.17f, 1.0f); // Position the bicycle frame
+    glScalef(0.8f, 0.05f, 0.05f); // Scale to make it look like a frame
+    glutSolidCube(0.5f); // Draw a solid cube for the frame
+    glPopMatrix();
+
+
+    // Set the color of the wheels to black
+    glColor3f(0, 0, 0);
+
+    // Draw the right wheel with rotation
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glTranslatef(0.15f, 0.082f, 1.0f);
+    glRotatef(rightWheelAngle, 0.0f, 1.0f, 0.0f); // Rotate the bicycle for steering
+    glutSolidTorus(0.025f, 0.06f, 6, 20);
+    glPopMatrix();
+
+    // Draw the left wheel with rotation
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glTranslatef(-0.15f, 0.082f, 1.0f);
+    glRotatef(leftWheelAngle, 0.0f, 1.0f, 0.0f); // Rotate the bicycle for steering
+    glutSolidTorus(0.025f, 0.06f, 6, 20);
+    glPopMatrix();
+
+
+    // Draw the handle of the bicycle
+    glColor3f(0.22f, 0.18f, 0.13f);
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glTranslatef(0.1f, 0.18f, 1.0); // Position the handle slightly higher to match the frame's new position
+    glRotatef(90.0f, 0.0f, 0.0f, 1.0f); // Rotate to make it look like a handle
+    glScalef(0.14f, 0.02f, 0.02f); // Scale to make it look like a handle
+    glutSolidCube(0.9f); // Draw a solid cube for the handle
+    glPopMatrix();
+
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glTranslatef(0.1f, 0.24f, 1.0f); // Position the handle slightly higher to match the frame's new position
+    glScalef(0.02f, 0.02f, 0.1f); // Scale to make it look like a handle
+    glutSolidCube(0.9f); // Draw a solid cube for the handle
+    glPopMatrix();
+
+    // Draw the seat of the bicycle
+    glColor3f(0.16f, 0.20f, 0.27f);
+    glPushMatrix();
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glTranslatef(-0.15f, 0.2f, 1.0f); // Position the seat slightly higher to match the frame's new position
+    glScalef(0.15f, 0.04f, 0.07f); // Scale to make it look like a seat
+    glutSolidCube(1.0f); // Draw a solid cube for the seat
     glPopMatrix();
 }
 
@@ -126,19 +250,20 @@ void drawBuilding() {
 
     glEnd();
 
-// Draw the door
+    // Draw the door
     drawDoor(-0.01f, 0.1f, 0.26f, -doorAngle);
 
-// Draw two windows on the first floor
+    // Draw two windows on the first floor
     for (int i = 0; i < 2; ++i) {
         drawWindow(-0.2f + i*0.4f, 0.25f, 0.26f, windowAngleFirstFloor);
     }
-// Draw two windows on the second floor
+    // Draw two windows on the second floor
     for (int i = 0; i < 2; ++i) {
         drawWindow(-0.2f + i*0.4f, 0.7f, 0.26f, windowAngleSecondFloor);
     }
 
 }
+
 float cameraPosX = 2.0f;
 float cameraPosY = 2.0f;
 float cameraPosZ = 2.0f;
@@ -147,6 +272,31 @@ void updateCameraPosition(float x, float y, float z) {
     cameraPosY = y;
     cameraPosZ = z;
 }
+
+void zoomCamera(unsigned char key, int x, int y){
+    switch (key) {
+        case 'z': // Zoom in
+            updateCameraPosition(cameraPosX, cameraPosY, cameraPosZ - 0.1f);
+            break;
+        case 'Z': // Zoom out
+            updateCameraPosition(cameraPosX, cameraPosY, cameraPosZ + 0.1f);
+            break;
+    }
+    glutPostRedisplay(); // Redraw the scene with updated camera position
+}
+
+void update(int value) {
+    if (animationEnabled) {
+        animationAngle += 1.0f;
+
+        if (animationAngle >= 360.0f){
+            animationAngle = 0.0f;
+        }
+        glutPostRedisplay();
+    }
+    glutTimerFunc(16, update, 0);
+}
+
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
@@ -155,7 +305,14 @@ void display() {
               0.0f, 0.0f, 0.0f,0.0f, 1.0f, 0.0f);
 
     drawBuilding();
-//    drawBicycle();
+    glPushMatrix();
+    glRotatef(bicycleRotation, 0.0, 1.0, 0.0);
+    drawBicycle();
+    glPopMatrix();
+
+    if (animationEnabled){
+        bicycleRotation += 1.0;
+    }
     glFlush();
     glutSwapBuffers();
 }
@@ -194,6 +351,7 @@ void specialKeyPress(int key, int x, int y) {
     }
     glutPostRedisplay();
 }
+
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
@@ -202,8 +360,13 @@ int main(int argc, char** argv) {
     init();
     // keyboard callback
     glutKeyboardFunc(keyPress);
+    // mouse callback
+
+    glutMouseFunc(mouse);
+    glutTimerFunc(20, update, 0);
     // Set up the display and reshape callbacks
     glutDisplayFunc(display);
+
     glutReshapeFunc(reshape);
     //specialKeyPress callback
     glutSpecialFunc(specialKeyPress);
